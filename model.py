d9dee521586431f2a4a2e73139bcc1f23368b8e4
paladuguramadevi <140633769+paladuguramadevi@users.noreply.github.com>
2024-02-12 14:56:48 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import pandas as pandas
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
